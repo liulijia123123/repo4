6b7a125561bee9139cb13de566190f7ce768d639
liulijia <18330229216@163.com>
2019-12-03 17:24:10 +0800

    CCCC

--- test_git/src/com/wskj/day01/Demo01.java
+++ test_git/src/com/wskj/day01/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("aaaa");
         System.out.println("BBB");
+        System.out.println("CCCCCCCCCC");
     }
 }
